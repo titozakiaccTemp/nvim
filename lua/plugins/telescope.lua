ff4b7de90cb7144173ccd2552af9de004c1c7d04
Rizkia Adhy Syahputra <rizkia.as.pac@gmail.com>
2024-02-05 23:21:05 +0700

    telescope grep from leader ft to leader fg

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -10,7 +10,7 @@ return {
 
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 			vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-			vim.keymap.set("n", "<leader>ft", function()
+			vim.keymap.set("n", "<leader>fg", function()
 				builtin.grep_string({ search = vim.fn.input("Grep > ") })
 			end)
 		end,
