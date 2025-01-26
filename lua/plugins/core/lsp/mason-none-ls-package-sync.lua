66a71a8a43ff4d4af1602704901d0069665e4d5c
Rizkia Adhy Syahputra <rizkia.as.pac@gmail.com>
2025-01-26 13:23:26 +0900

    -

--- lua/plugins/core/lsp/mason-none-ls-package-sync.lua
+++ lua/plugins/core/lsp/mason-none-ls-package-sync.lua
@@ -1,27 +1,24 @@
 return {
-    "jay-babu/mason-null-ls.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-      "williamboman/mason.nvim",
-      "nvimtools/none-ls.nvim",
-    },
-    config = function()
-
+  "jay-babu/mason-null-ls.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "williamboman/mason.nvim",
+    "nvimtools/none-ls.nvim",
+  },
+  config = function()
     require("mason-null-ls").setup({
-        ensure_installed = {
-            -- Opt to list sources here, when available in mason.
-            -- Note: this plugin uses the null-ls source names in the APIs it exposes - not mason.nvim package names.⇧
+      ensure_installed = {
+        -- Opt to list sources here, when available in mason.
+        -- Note: this plugin uses the null-ls source names in the APIs it exposes - not mason.nvim package names.⇧
         "stylua",
-        },
-        automatic_installation = false,
-        handlers = {},
+      },
+      automatic_installation = false,
+      handlers = {},
     })
     require("null-ls").setup({
-        sources = {
-            -- Anything not supported by mason.
-        }
+      sources = {
+        -- Anything not supported by mason.
+      },
     })
-
-
   end,
 }
