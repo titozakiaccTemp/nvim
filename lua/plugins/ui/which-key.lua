89d9230fd35d606d99327ae99eefe03e819bfa35
Rizkia Adhy Syahputra <rizkia.as.pac@gmail.com>
2025-01-26 12:54:30 +0900

    refactor: update key groupings and disable icons in  config

--- lua/plugins/ui/which-key.lua
+++ lua/plugins/ui/which-key.lua
@@ -7,33 +7,34 @@ return {
     -- refer to the configuration section below
     icons = {
       separator = ":",
-      mappings = false -- disable icons
+      mappings = false, -- disable icons
     },
     spec = {
       {
-        { "<leader>t", group = "NETRW" },
+        { "<leader>t",  group = "NETRW" },
+        { "<leader>u",  group = "Window options" },
 
-        { "<leader>c", group = "Code" },
+        { "<leader>c",  group = "Code" },
         { "<leader>cs", group = "Rename all every same word like in under cursor" },
         { "<leader>cx", group = "Make current buffer executable" },
 
-        { "<leader>y", group = "Yank options" },
-        { "<leader>Y", group = "Yank to system clipboard" },
+        { "<leader>y",  group = "Yank options" },
+        { "<leader>Y",  group = "Yank to system clipboard" },
 
-        { "<leader>m", group = "Miscellaneous" },
+        { "<leader>m",  group = "Miscellaneous" },
         { "<leader>ml", group = "LSP things" },
         { "<leader>mu", group = "UI things" },
 
-        { "<leader>f", group = "Telescope" },
-        { "<leader>s", group = "Search" },
+        { "<leader>f",  group = "Telescope" },
+        { "<leader>s",  group = "Search" },
       },
       {
         mode = { "v" },
         { "<leader>y", group = "Yank to system clipboard" },
         { "<leader>c", group = "Code" },
         { "<leader>u", group = "Screenshot" },
-      }
-    }
+      },
+    },
   },
   keys = {
     {
