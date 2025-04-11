73e8045dd58c86bf264f4e9fb8d98442414a7c91
Rizkia Adhy Syahputra <rizkia.as.pac@gmail.com>
2025-04-12 00:20:56 +0900

    feat(ui): enhance colorscheme options and add scrollEOF plugin
    - Added Catppuccin theme with custom palette overrides
    - Updated colorscheme module to include Catppuccin alongside Tokyo Night
    - Introduced scrollEOF.nvim plugin for smooth end-of-file scrolling
    - Extended which-key config with new key labels and grouped mappings
    - DELETE flutter-tools and undotree file

--- lua/plugins/ui/colorscheme.lua
+++ lua/plugins/ui/colorscheme.lua
@@ -1,11 +1,37 @@
 return {
-	-- tokyonight
 	{
 		"folke/tokyonight.nvim",
 		lazy = true,
 		opts = {
 			style = "moon",
 			integrations = { blink_cmp = true },
+		},
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		opts = {
+			-- flavour = "latte",
+			flavour = "macchiato",
+			color_overrides = {
+				macchiato = {
+					overlay2 = "#9399b2",
+					overlay1 = "#7f849c",
+					overlay0 = "#6c7086",
+					surface2 = "#585b70",
+					surface1 = "#45475a",
+					-- surface0 = "#313244", -- ori
+					surface0 = "#272735", -- bit darker
+					base = "#1e1e2e",
+					mantle = "#181825",
+					crust = "#11111b",
+				},
+				latte = {
+					green = "#439d2f",
+				},
+			},
+			-- ref : catppuccin/nvim/lua/catppuccin/palettes/latte.lua
 		},
 	},
 }
--- lua/plugins/ui/which-key.lua
+++ lua/plugins/ui/which-key.lua
@@ -1,13 +1,47 @@
 return {
 	"folke/which-key.nvim",
 	event = "VeryLazy",
+	-- event = 'VimEnter', -- Sets the loading event to 'VimEnter'
 	opts = {
 		-- your configuration comes here
 		-- or leave it empty to use the default settings
 		-- refer to the configuration section below
+		delay = 0,
 		icons = {
 			separator = ":",
 			mappings = false, -- disable icons
+			keys = {
+				Up = "<Up> ",
+				Down = "<Down> ",
+				Left = "<Left> ",
+				Right = "<Right> ",
+				C = "<C-…> ",
+				M = "<M-…> ",
+				D = "<D-…> ",
+				S = "<S-…> ",
+				CR = "<CR> ",
+				Esc = "<Esc> ",
+				ScrollWheelDown = "<ScrollWheelDown> ",
+				ScrollWheelUp = "<ScrollWheelUp> ",
+				NL = "<NL> ",
+				-- BS = '<BS> ',
+				BS = "<Backspace> ",
+				-- Space = '<Space> ',
+				Space = "",
+				Tab = "<Tab> ",
+				F1 = "<F1>",
+				F2 = "<F2>",
+				F3 = "<F3>",
+				F4 = "<F4>",
+				F5 = "<F5>",
+				F6 = "<F6>",
+				F7 = "<F7>",
+				F8 = "<F8>",
+				F9 = "<F9>",
+				F10 = "<F10>",
+				F11 = "<F11>",
+				F12 = "<F12>",
+			},
 		},
 		spec = {
 			{
@@ -17,6 +51,7 @@ return {
 				{ "<leader>c", group = "Code" },
 				{ "<leader>cs", group = "Rename all every same word like in under cursor" },
 				{ "<leader>cx", group = "Make current buffer executable" },
+				{ "<leader>cp", group = "Format" },
 
 				{ "<leader>y", group = "Yank options" },
 				{ "<leader>Y", group = "Yank to system clipboard" },
@@ -24,9 +59,11 @@ return {
 				{ "<leader>m", group = "Miscellaneous" },
 				{ "<leader>ml", group = "LSP things" },
 				{ "<leader>mu", group = "UI things" },
+				{ "<leader>mc", group = "Neovim Config" },
 
 				{ "<leader>f", group = "Telescope" },
 				{ "<leader>s", group = "Search" },
+				{ "<leader>x", group = "Fix list" },
 			},
 			{
 				mode = { "v" },
