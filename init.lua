5009e8788dbae9bc3e35949b7bcf132e93af73cd
Rizkia Adhy Syahputra <rizkia.as.pac@gmail.com>
2025-04-12 00:26:54 +0900

    refactor(config): clean up init file and switch to user-config
    - Removed old and commented-out configuration setup.
    - Updated `init.lua` to load `user-config` instead of legacy code.
    - Updated `lazy-lock.json` with new plugins and plugin version changes.

--- init.lua
+++ init.lua
@@ -1,20 +1,2 @@
 require("config")
---require("rizkia")
--- require("config")
---local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
---if not vim.loop.fs_stat(lazypath) then
---	vim.fn.system({
---		"git",
---		"clone",
---		"--filter=blob:none",
---		"https://github.com/folke/lazy.nvim.git",
---		"--branch=stable", -- latest stable gelease
---		lazypath,
---	})
---end
---vim.opt.rtp:prepend(lazypath)
---
---local opts = {}
-
--- require("vim-options")
--- require("lazy").setup("plugins")
+require("user-config")
